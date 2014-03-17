7d4b9b549b4287d008ed199ab1fe281a6fba7f16
mxlnu1 <mxlnu1@ualr.edu>
2014-03-17 16:53:05 -0500

    loading image
    loading image

--- OnlineArkansas/Scripts/training.js
+++ OnlineArkansas/Scripts/training.js
@@ -93,7 +93,7 @@
             }
         },
         submitHandler: function (form) {
-
+            $(".ajax-loader").html("<img src='/img/loader.gif'>");
             $.ajax({
                 url: '../Home/RegistrationForm',
                 type: "POST",
@@ -117,6 +117,7 @@
                     courseFee: courseFee
                 }),
                 success: function (result) {
+                    $(".ajax-loader").html('');
                     $("#fundamentalsRegistration").dialog("close");
                     alert("Congratulations! " + result.firstName + ". You have successfully registered. Invoice and location instructions will be mailed or faxed to you prior to the class.");
                 },
